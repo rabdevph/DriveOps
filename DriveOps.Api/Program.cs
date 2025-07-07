926f3a708e1d0af0d2f589f8487a6a41bd3690b6
Ralph Arsy Bauto <rab.devph@gmail.com>
2025-07-07 17:53:16 +0400

    chore(api): load .env file in development and configure PostgreSQL connection

--- DriveOps.Api/Program.cs
+++ DriveOps.Api/Program.cs
@@ -1,8 +1,21 @@
+using DotNetEnv;
+using DriveOps.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+if (builder.Environment.IsDevelopment())
+{
+    Env.Load();
+}
+
+var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+builder.Services.AddDbContext<DriveOpsContext>(options =>
+    options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
