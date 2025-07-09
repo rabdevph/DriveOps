6eeef8a0d42880c82b5b0d110c5814de4a78f23e
Ralph Arsy Bauto <rab.devph@gmail.com>
2025-07-09 12:05:52 +0400

    feat(api): add Swagger support and enable XML comments for API docs

--- DriveOps.Api/Program.cs
+++ DriveOps.Api/Program.cs
@@ -1,28 +1,32 @@
+using System.Reflection;
 using DotNetEnv;
 using DriveOps.Api.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
 
 if (builder.Environment.IsDevelopment())
 {
     Env.Load();
 }
-
 var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL");
-
 builder.Services.AddDbContext<DriveOpsContext>(options =>
     options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
