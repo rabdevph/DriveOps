d778c7ee11026976b2a39efe429d65123de4b42a
Ralph Arsy Bauto <rab.devph@gmail.com>
2025-07-11 15:24:15 +0400

    refactor(api): wrap GetByIdAsync result in ServiceResult for consistent error handling

--- DriveOps.Api/Interfaces/ICustomerService.cs
+++ DriveOps.Api/Interfaces/ICustomerService.cs
@@ -7,7 +7,7 @@ namespace DriveOps.Api.Interfaces;
 public interface ICustomerService
 {
     Task<CustomerPaginatedResultDto<CustomerDetailsDto>> GetAllAsync(CustomerType? type, int page, int pageSize);
-    Task<CustomerDetailsDto?> GetByIdAsync(int id, bool onlyCurrent);
+    Task<ServiceResult<CustomerDetailsDto>> GetByIdAsync(int id, bool onlyCurrent);
     Task<ServiceResult<CustomerDetailsDto>> CreateAsync(CustomerCreateDto dto);
     Task<ServiceResult<CustomerDetailsDto>> UpdateDetailsAsync(int id, CustomerDetailsUpdateDto dto);
     Task<ServiceResult<CustomerDetailsDto>> UpdateStatusAsync(int id, CustomerStatusUpdateDto dto);
