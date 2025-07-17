2b52d8b9689a47c2575b6583b137b4809c54eee2
Ralph Arsy Bauto <rab.devph@gmail.com>
2025-07-17 12:55:38 +0400

    feat(shared): add mechanic status enum

---
+++ DriveOps.Shared/Enums/MechanicStatus.cs
@@ -0,0 +1,10 @@
+using System.Text.Json.Serialization;
+
+namespace DriveOps.Shared.Enums;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum MechanicStatus
+{
+    Active,
+    Inactive
+}
